2df8c3bb81ab0b608d2b339a9c432d1811c1480f
Brian Emilius <brian@brianemilius.com>
2022-12-05 13:20:34 +0100

    v1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Express Eksempel
+
+blah blah
+
+[Changelog](./CHANGELOG.md)
---
+++ middlewares/auth.js
@@ -0,0 +1,11 @@
+// middlewares/auth.js
+export default function authorization(request, response, next) {
+	console.log(request.headers)
+	if (request.headers.authorization !== "1234") {
+		response.status(401)
+		response.end()
+		return
+	}
+
+	next()
+}
