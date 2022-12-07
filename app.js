9e3fa0d370f00d53d46852c150c6afdca92e4ece
Brian Emilius <be@rts.dk>
2022-12-07 14:26:10 +0100

    add routes and filehandling middleware

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express"
-import oste from "./routes/oste.js"
+import cheeses from "./routes/cheeses/index.js"
 
 const app = express()
 
 app.use(express.static("./public"))
-oste(app)
+cheeses(app)
 
 app.listen(1337, function() {
 	console.log("The App is listening on port 1337")
