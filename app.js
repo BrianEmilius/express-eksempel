65a975af5a46057bec61e1d2992a71c2f8f0ba7b
Brian Emilius <be@rts.dk>
2022-12-13 11:29:46 +0100

    update authentication middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cheeses from "./routes/cheeses/index.js"
 import users from "./routes/users/index.js"
+import auth from "./routes/auth/index.js"
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 cheeses(app)
 users(app)
+auth(app)
 
 app.listen(1337, function() {
 	console.log("The App is listening on port 1337")
